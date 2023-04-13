97cef5dc53917cddd85eb42203e32ef4f7e85264
Ivan Baksheev <zpix-dev@list.ru>
2023-04-13 23:51:39 +0700

    fix: fix

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,3 @@
-// import { Worker } from './Worker';
-// import { closeConnection } from './utils/dbClient';
-
-// const PERIOD = 60 * 60 * 1000;
-
-// const sleep = (ms: number): Promise<void> => {
-//   return new Promise(resolve => setTimeout(resolve, ms));
-// };
-
-// const main = async (): Promise<void> => {
-//   const worker = new Worker();
-//   for (let i = 0; ; i++) {
-//     await worker.doAll({
-//       reloadContracts: i % 10 === 0
-//     });
-//     const nextTime = new Date(Date.now() + PERIOD);
-//     console.log(`Next time at ${nextTime}`);
-//     await sleep(PERIOD);
-//   }
-//   await closeConnection();
-// };
-
-// main();
-
 export * from './Worker';
 export * from './types';
 export * from './config';
